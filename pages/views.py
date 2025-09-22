a21d68d8ee25f612517844a6687286f9583cd3a6
apollotravisdulnuan <apollotravis.dulnuan@evsu.edu.ph>
2025-09-22 22:06:14 +0800

    WEEk1 initial django scaffold (pages)

---
+++ pages/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    ctx = {
+        'course': 'IT373 - websystem and Technologies 2',
+        'week1': 1,
+    }
+    return render(request, 'pages/home.html', ctx)
+def hello(request):
+    return render(request, 'pages/hello.html', {})
+def about(request):
+    return render(request, 'pages/about.html', {})
